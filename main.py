e7b7372a560c445e484371f8446ea6769c5266f5
YaroslavPopov2008 <yarik.popov3112@gmail.com>
2025-01-28 19:31:36 +0300

    Added cursor Added if __name__ == "__main__"

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def check_button_click(pos):
         return 'levels_data/LVL1_MAP'
     elif 350 <= pos[0] <= 540 and 410 <= pos[1] <= 570:
         return 'levels_data/LVL2_MAP'
-    elif 570 <= pos[0] <= 760 and 410 <= pos[1] <= 570:
+    elif 760 >= pos[0] >= 570 >= pos[1] >= 410:
         return 'levels_data/LVL3_MAP'
     return None
 
@@ -62,37 +62,6 @@ def randomize_pipes(map_data):
                 map_data[i][j] = cell
 
 
-pygame.init()
-
-SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
-PIPE_SIZE = 100
-FPS = 60
-WHITE = (255, 255, 255)
-
-pipe_images = [
-    pygame.image.load('data/plumb1 (1).png'),
-    pygame.image.load('data/plumb6.png'),
-    pygame.image.load('data/plumb2 (1).png'),
-    pygame.image.load('data/plumb3 (1).png'),
-    pygame.image.load('data/plumb4 (1).png'),
-    pygame.image.load('data/plumb5 (1).png'),
-    pygame.image.load('data/plumb_start.png'),
-    pygame.image.load('data/plumb_end.png')
-]
-fon = pygame.image.load('data/top-view-soil (1).jpg')
-background_image = pygame.transform.scale(fon, (SCREEN_WIDTH, SCREEN_HEIGHT))
-
-pipe_images = [pygame.transform.scale(img, (PIPE_SIZE, PIPE_SIZE)) for img in pipe_images]
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('Трубопроводчик')
-
-font = pygame.font.Font(None, 70)
-text_lvl1 = font.render('Level 1', True, WHITE)
-text_lvl2 = font.render('Level 2', True, WHITE)
-text_lvl3 = font.render('Level 3', True, WHITE)
-
-
 def render_map(map_data, pipe_images):
     pipe_dict = {
         '@': pipe_images[3],  # ПОВОРОТ ЛЕВОНИЗ
@@ -119,35 +88,68 @@ def render_map(map_data, pipe_images):
     screen.blit(text_lvl3, (580, 420))
 
 
-current_map_file = 'levels_data/LVL1_MAP'
-clock = pygame.time.Clock()
-running = True
-map_data = load_map(current_map_file)
-randomize_pipes(map_data)
+if __name__ == "__main__":
+    pygame.init()
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:  # левая кнопка мыши
-                row, col = get_clicked_pipe(event.pos)
-                if 0 <= row < len(map_data) and 0 <= col < len(map_data[row]):
-                    map_data[row][col] = rotate_pipe(map_data[row][col])
+    SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
+    PIPE_SIZE = 100
+    FPS = 60
+    WHITE = (255, 255, 255)
 
-            new_map = check_button_click(event.pos)
-            if new_map:
-                save_map(current_map_file, map_data)  # сохраняем текущую карту перед загрузкой новой
-                current_map_file = new_map
-                map_data = load_map(current_map_file)
-                randomize_pipes(map_data)  # случайное поворачивание труб при загрузке нового уровня
+    pipe_images = [
+        pygame.image.load('data/plumb1 (1).png'),
+        pygame.image.load('data/plumb6.png'),
+        pygame.image.load('data/plumb2 (1).png'),
+        pygame.image.load('data/plumb3 (1).png'),
+        pygame.image.load('data/plumb4 (1).png'),
+        pygame.image.load('data/plumb5 (1).png'),
+        pygame.image.load('data/plumb_start.png'),
+        pygame.image.load('data/plumb_end.png')
+    ]
+    fon = pygame.image.load('data/top-view-soil (1).jpg')
+    cursor = pygame.image.load('data/wrench.jpg')  # TODO: поменять формат курсора на пнг и добавить convert_alpha()
+    background_image = pygame.transform.scale(fon, (SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    screen.blit(background_image, (0, 0))
-    render_map(map_data, pipe_images)
+    pipe_images = [pygame.transform.scale(img, (PIPE_SIZE, PIPE_SIZE)) for img in pipe_images]
 
-    pygame.display.flip()
-    clock.tick(FPS)
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption('Трубопроводчик')
 
-pygame.quit()
-restore_original_maps()
-sys.exit()
+    font = pygame.font.Font(None, 70)
+    text_lvl1 = font.render('Level 1', True, WHITE)
+    text_lvl2 = font.render('Level 2', True, WHITE)
+    text_lvl3 = font.render('Level 3', True, WHITE)
+
+    current_map_file = 'levels_data/LVL1_MAP'
+    clock = pygame.time.Clock()
+    running = True
+    map_data = load_map(current_map_file)
+    randomize_pipes(map_data)
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:  # левая кнопка мыши
+                    row, col = get_clicked_pipe(event.pos)
+                    if 0 <= row < len(map_data) and 0 <= col < len(map_data[row]):
+                        map_data[row][col] = rotate_pipe(map_data[row][col])
+
+                new_map = check_button_click(event.pos)
+                if new_map:
+                    save_map(current_map_file, map_data)  # сохраняем текущую карту перед загрузкой новой
+                    current_map_file = new_map
+                    map_data = load_map(current_map_file)
+                    randomize_pipes(map_data)  # случайное поворачивание труб при загрузке нового уровня
+            if pygame.mouse.get_focused():
+                pygame.mouse.set_cursor((0, 0), cursor)
+        screen.blit(background_image, (0, 0))
+        render_map(map_data, pipe_images)
+
+        pygame.display.flip()
+        clock.tick(FPS)
+
+    pygame.quit()
+    restore_original_maps()
+    sys.exit()
