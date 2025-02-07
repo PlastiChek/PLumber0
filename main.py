8f8e6d2681de6f59876c5d25647cef5cda6528ab
evilmilk2008 <yarik.popov3112@gmail.com>
2025-02-07 16:15:39 +0300

    Added gameplay + presentation

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def start_screen():
         string_rendered = font.render(line, 1, pygame.Color(WHITE))
         intro_rect = string_rendered.get_rect()
         text_coord += 10
-        intro_rect.top = text_coord + 50
+        intro_rect.top = text_coord + 150
         intro_rect.x = 150
         text_coord += intro_rect.height
         screen.blit(string_rendered, intro_rect)
